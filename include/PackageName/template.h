d83da13cf70a78a44a938b6ccf68427a0181f431
Vamsi <vamsikalagaturu@gmail.com>
2024-11-25 17:01:25 +0100

    simplified pre-commit hook

--- include/PackageName/template.h
+++ include/PackageName/template.h
@@ -1,10 +1,22 @@
 #ifndef PACKAGENAME_TEMPLATE_H
 #define PACKAGENAME_TEMPLATE_H
 
-// Function declaration for addition
+/**
+ * @brief Add two integers
+ *
+ * @param pA First integer
+ * @param pB Second integer
+ * @return int Sum of the two integers
+ */
 int add(int pA, int pB);
 
-// Function declaration for subtraction
+/**
+ * @brief Subtract two integers
+ *
+ * @param pA First integer
+ * @param pB Second integer
+ * @return int Difference of the two integers
+ */
 int subtract(int pA, int pB);
 
 int multiply(int pA, int pB)
